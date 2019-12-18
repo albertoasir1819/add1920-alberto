10652a34b7463c6d18aadf34879ebca922abf2df
albertoasir1819 <albertoasir1819@gmail.com>
2019-12-18 09:46:54 +0000

    update

--- trim1/u5/a2/set_net.rb
+++ trim1/u5/a2/set_net.rb
@@ -1,27 +1,39 @@
 #!/usr/bin/env ruby
 
+require 'colorize'
+
 puts "
 -------------------------------------------------------
        1. Aula 109 estática
        2. Resetear
        3. Casa estática
        4. DHCP
+       5. Refresh DHCP
 -------------------------------------------------------
+Press Enter to exit
       "
+
+def reset
+  system("sudo ifdown eth0")
+  system("sudo ifup eth0")
+end
+
 option = gets.chop
 if option == "1"
-  system("sudo cp ip-aula109 /etc/sysconfig/network/ifcfg-eth0")  
-  system("sudo ifdown eth0")
-  system("sudo ifup eth0") 	
+  system("sudo cp ip-aula109 /etc/sysconfig/network/ifcfg-eth0")
+  puts "Configurando...".colorize(:green)
+  reset
 elsif option == "2"
-  system("sudo ifdown eth0")
-  system("sudo ifup eth0") 
+  reset
 elsif option == "3"
   system("sudo cp ip-casa /etc/sysconfig/network/ifcfg-eth0")
-  system("sudo ifdown eth0")
-  system("sudo ifup eth0")
+  puts "Configurando...".colorize(:green)
+  reset
 elsif option == "4"
   system("sudo cp dhcp /etc/sysconfig/network/ifcfg-eth0")
-  system("sudo ifdown eth0")
-  system("sudo ifup eth0")
+  puts "Configurando...".colorize(:green)
+  reset
+elsif option == "5"
+  system("sudo dhclient -r eth0")
+  puts "Configurando...".colorize(:green)
 end
